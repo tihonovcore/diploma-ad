01b23fe1b4847604b4b49eae7820271fea6eda66
Tikhonov Vitaly <tihonovcore@gmail.com>
2022-03-19 14:20:07 +0300

    Implement mvp compilation process

--- src/main/kotlin/com/tihonovcore/diploma/ad/model/CompilerConfiguration.kt
+++ src/main/kotlin/com/tihonovcore/diploma/ad/model/CompilerConfiguration.kt
@@ -1,12 +1,29 @@
 package com.tihonovcore.diploma.ad.model
 
 import java.io.File
-import java.nio.file.Path
 
 class CompilerConfiguration(
-    val pathToKotlinCompilerJar: Path
+    //TODO: use jar
+    val pathToKotlincJvmFile: String
 ) {
     fun compile(file: File): CompilationResult {
-        TODO()
+        val beginTime = System.currentTimeMillis()
+        val process = ProcessBuilder(pathToKotlincJvmFile, file.absolutePath).start()
+        val output = process.inputStream.reader(Charsets.UTF_8).use {
+            it.readText()
+        }
+
+        val errorOutput = process.errorStream.reader(Charsets.UTF_8).use { it.readText() }
+
+        val success = process.waitFor() == 0
+
+        return CompilationResult(
+            compilerConfiguration = this,
+            file,
+            success,
+            System.currentTimeMillis() - beginTime,
+            100,
+            output + errorOutput
+        )
     }
 }
